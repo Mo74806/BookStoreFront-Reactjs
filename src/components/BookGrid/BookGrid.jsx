f856861dc84b6f87228fdaf1bcec727154e9f854
Mostafa Khaled <56610429+Mo74806@users.noreply.github.com>
2023-02-08 15:53:37 +0200

    editing 1

--- src/components/BookGrid/BookGrid.jsx
+++ src/components/BookGrid/BookGrid.jsx
@@ -35,7 +35,7 @@ export default function BookGrid(props) {
             </div>
           );
         })}
-      {!isLoading && books && books.length <= 0 && (
+      {!isLoading && books.length <= 0 && (
         <div
           className={`mx-auto text-center   py-5 fw-semibold fs-1 ${classes.imgNotfound}`}
         >
